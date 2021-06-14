de42cedeb22e93d2758685ef223f1fe611beada0
MJ <mohamed.aref32@gmail.com>
2021-06-13 19:04:13 -0500

    Starting the header and hero component

---
+++ src/components/Header/Header.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+
+const Navbar = styled.nav`
+height:60px;
+background: #000;
+padding: 0rem calc((100vw -1300px) / 2);
+display: flex;
+justify-content: space-between;
+align-items: center;
+
+`;
+
+const Header = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Header
